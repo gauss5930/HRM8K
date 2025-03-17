737c5f0d577a4609815758985f8104da26e2143a
Hyunwoo Ko <kopilot100@gmail.com>
2025-03-17 21:39:55 +0900

    Update generate.py

--- src/generate.py
+++ src/generate.py
@@ -6,6 +6,12 @@ from vllm import LLM, SamplingParams
 from tqdm.auto import tqdm
 from jinja2.exceptions import TemplateError
 
+def safe_parse(text):
+    try:
+        return text.choices[0].message.content
+    except:
+        return None
+
 def query_mapping(prompt_id):
     if prompt_id in ["k2k", "e2k"]:
         return prompts["ko"]
@@ -35,7 +41,10 @@ def generate_queries_clp(df, model_name):
     return qrys
 
 def generate_queries(df, model_name, prompt_id):
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    try:        
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+    except:
+        tokenizer = ""
     qrys = []
     system_message = prompts["plug_system"] if prompt_id == "plug" else ""
     
@@ -43,7 +52,7 @@ def generate_queries(df, model_name, prompt_id):
         question = row.original if prompt_id in ["en",'oasst_en', 'e2e', 'e2k'] else row.question
         msg = prompts[prompt_id].replace("{instruction}", question) if 'oasst' in prompt_id else " ".join([question, query_mapping(prompt_id)])
 
-        if model_name not in litellm_models:
+        if (model_name not in litellm_models) or ("openrouter" in model_name):
             try:
                 if system_message:
                     messages = [{"role": "system", "content": system_message}]
@@ -60,10 +69,11 @@ def generate_queries(df, model_name, prompt_id):
                 else:
                     print(f"An error occurred: {e}")
         else:
-            qry = [
-                    {"role": "system", "content": system_message},
-                    {"role": "user", "content": msg}
-                ]
+            if system_message:
+                qry = [{"role": "system", "content": system_message}]
+            else:
+                qry = []
+            qry.append({"role": "user", "content": msg})
 
         qrys.append(qry)
 
@@ -71,16 +81,16 @@ def generate_queries(df, model_name, prompt_id):
 
 
 def generate_solution(prompt_id, model_name, temperature, p, dfs):
-    if model_name not in litellm_models:
+    if (model_name not in litellm_models) or ("openrouter" in model_name):
         model, params = load_model(model_name, temperature, p)
 
     df_results = {}
     for k, df in tqdm(dfs.items(),total=len(dfs)):
         if prompt_id != "clp":
             prompts = generate_queries(df, model_name, prompt_id)
-            if model_name in litellm_models:
+            if (model_name not in litellm_models) or ("openrouter" in model_name):
                 responses = batch_completion(model=model_name, messages=prompts, temperature=temperature, top_p=p, max_tokens=2048)
-                outputs = [resp.choices[0].message.content for resp in responses]
+                outputs = [safe_parse(resp) for resp in responses]
             else:
                 responses = model.generate(prompts, params)
                 outputs = [output.outputs[0].text for output in responses]
@@ -88,18 +98,18 @@ def generate_solution(prompt_id, model_name, temperature, p, dfs):
 
         else:
             align_prompts = generate_queries_clp(df, model_name)
-            if model_name in litellm_models:
+            if (model_name not in litellm_models) or ("openrouter" in model_name):
                 responses = batch_completion(model=model_name, messages=align_prompts, temperature=temperature, top_p=p, max_tokens=2048)
-                align_outputs = [resp.choices[0].message.content for resp in responses]
+                align_outputs = [safe_parse(resp) for resp in responses]
             else:
                 responses = model.generate(align_prompts, params)
                 align_outputs = [output.outputs[0].text for output in responses]
             df["alignment"] = align_outputs
 
             solution_prompts = generate_queries_clp(df, model_name)
-            if model_name in litellm_models:
+            if (model_name not in litellm_models) or ("openrouter" in model_name):
                 responses = batch_completion(model=model_name, messages=solution_prompts, temperature=temperature, top_p=p, max_tokens=2048)
-                solution_outputs = [resp.choices[0].message.content for resp in responses]
+                solution_outputs = [safe_parse(resp) for resp in responses]
             else:
                 responses = model.generate(solution_prompts, params)
                 solution_outputs = [output.outputs[0].text for output in responses]
